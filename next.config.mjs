5330299a05b63870780facb0332538e8a3a5d0a0
Ramy Simpson <ramysimpson@mac.mynetworksettings.com>
2026-03-05 20:31:30 -0500

    Simplify next.config and update lock

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,2 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbo: false,
-  },
-};
-
-export default nextConfig;
+export default {};
